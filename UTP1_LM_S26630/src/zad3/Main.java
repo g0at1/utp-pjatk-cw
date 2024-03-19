0e4c99bf80fa3cce786bbb453dd9be78fa0df1de
Michał Lendzion <95431893+g0at1@users.noreply.github.com>
2024-03-19 19:00:02 +0100

    UTP1_LM_S26630

---
+++ UTP1_LM_S26630/src/zad3/Main.java
@@ -0,0 +1,33 @@
+/**
+ *
+ *  @author Lendzion Michał S26630
+ *
+ */
+
+package zad3;
+
+
+/*<-- niezbędne importy */
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Lista destynacji: port_wylotu port_przylotu cena_EUR 
+    List<String> dest = Arrays.asList(
+      "bleble bleble 2000",
+      "WAW HAV 1200",
+      "xxx yyy 789",
+      "WAW DPS 2000",
+      "WAW HKT 1000"
+    );
+    double ratePLNvsEUR = 4.30;
+    List<String> result = 
+    /*<-- tu należy dopisać fragment
+     * przy czym nie wolno używać żadnych własnych klas, jak np. ListCreator
+     * ani też żadnych własnych interfejsów
+     * Podpowiedź: należy użyć strumieni
+     */
+
+    for (String r : result) System.out.println(r);
+  }
+}
