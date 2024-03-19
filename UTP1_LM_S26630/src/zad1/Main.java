c89ded76cecb9584e741303c15dda8dabf239178
Michał Lendzion <95431893+g0at1@users.noreply.github.com>
2024-03-19 19:05:38 +0100

    feature: Whole UTP1_LM_S26630

---
+++ UTP1_LM_S26630/src/zad1/Main.java
@@ -0,0 +1,57 @@
+/**
+ *
+ *  @author Lendzion Michał S26630
+ *
+ */
+
+package zad1;
+
+
+
+import java.util.*;
+
+public class Main {
+  public Main() {
+   List<Integer> src1 = Arrays.asList(1, 7, 9, 11, 12); /*<-- tu dopisać inicjację elementów listy */
+    System.out.println(test1(src1));
+
+    List<String> src2 = Arrays.asList("a", "zzzz", "vvvvvvv" ); /*<-- tu dopisać inicjację elementów listy */
+    System.out.println(test2(src2));
+  }
+
+  public List<Integer> test1(List<Integer> src) {
+    Selector<Integer> sel = new Selector<Integer>() {
+      @Override
+      public boolean select(Integer item) {
+        return item < 10;
+      }
+    };                                                                  /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
+    Mapper<Integer, Integer> map = new Mapper<Integer, Integer>() {
+      @Override
+      public Integer map(Integer item) {
+        return item + 10;
+      }
+    };                                                                  /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
+    return ListCreator.collectFrom(src).when(sel).mapEvery(map);        /*<-- zwrot wyniku uzyskanego przez wywołanie statycznej metody klasy ListCreator: */
+  }
+
+  public List<Integer> test2(List<String> src) {
+    Selector<String> sel = new Selector<String>() {
+      @Override
+      public boolean select(String item) {
+        return item.length() > 3;
+      }
+    };                                                                  /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
+    Mapper<String, Integer> map = new Mapper<String, Integer>() {
+      @Override
+      public Integer map(String item) {
+        return item.length() + 10;
+      }
+    };                                                                  /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
+    return ListCreator.collectFrom(src).when(sel).mapEvery(map);        /*<-- zwrot wyniku  uzyskanego przez wywołanie statycznej metody klasy ListCreator: */
+  }
+
+  public static void main(String[] args) {
+    new Main();
+  }
+}
---
+++ UTP1_LM_S26630/src/zad2/Main.java
@@ -0,0 +1,39 @@
+/**
+ *
+ *  @author Lendzion Michał S26630
+ *
+ */
+
+package zad2;
+
+
+import java.util.*;
+
+public class Main {
+
+  static List<String> getPricesInPLN(List<String> destinations, double xrate) {
+    return ListCreator.collectFrom(destinations)
+            .when(x -> x.startsWith("WAW"))
+            .mapEvery(x -> {
+                String[] xArr = x.split(" ");
+                String destination = xArr[1];
+                int priceInEUR = Integer.parseInt(xArr[2]);
+                int priceInPLN = (int)(priceInEUR * xrate);
+                return String.format("to %s - price in PLN: %d", destination, priceInPLN);
+            });
+  }
+
+  public static void main(String[] args) {
+    // Lista destynacji: port_wylotu port_przylotu cena_EUR 
+    List<String> dest = Arrays.asList(
+      "bleble bleble 2000",
+      "WAW HAV 1200",
+      "xxx yyy 789",
+      "WAW DPS 2000",
+      "WAW HKT 1000"
+    );
+    double ratePLNvsEUR = 4.30;
+    List<String> result = getPricesInPLN(dest, ratePLNvsEUR);
+    for (String r : result) System.out.println(r);
+  }
+}
