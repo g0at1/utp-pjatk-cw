c8bbe240c604a3fd5980ad03ca1f048bfcbb05f2
g0at1 <michal.lendzion@vp.pl>
2024-05-26 20:36:40 +0200

    UTP_7(6) folder

---
+++ UTP7_LM_S26630/src/zad1/Database.java
@@ -0,0 +1,35 @@
+package zad1;
+
+import java.sql.*;
+import java.util.List;
+import javax.swing.*;
+
+public class Database {
+  private String url;
+  private TravelData travelData;
+
+  public Database(String url, TravelData travelData) {
+    this.url = url;
+    this.travelData = travelData;
+  }
+
+  public void create() {
+  }
+
+  public void showGui() {
+    SwingUtilities.invokeLater(() -> {
+      JFrame frame = new JFrame("Travel Offers");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      String[] columnNames = { "Country", "Start Date", "End Date",
+          "Place", "Price", "Currency" };
+      String[][] data = retrieveOffers();
+
+      JTable table = new JTable(data, columnNames);
+      frame.add(new JScrollPane(table));
+
+      frame.pack();
+      frame.setVisible(true);
+    });
+  }
+}
---
+++ UTP7_LM_S26630/src/zad1/TravelData.java
@@ -0,0 +1,17 @@
+package zad1;
+
+import java.io.File;
+import java.util.List;
+
+public class TravelData {
+  private File dataDir;
+
+  public TravelData(File dataDir) {
+    this.dataDir = dataDir;
+  }
+
+  public List<String> getOffersDescriptionsList(String locale,
+      String dateFormat) {
+    return null;
+  }
+}
